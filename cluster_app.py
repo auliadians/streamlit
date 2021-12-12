7255cda0f022c7a3d7c9a73ca4f102a087a78c52
AULIA DIAN SAFITRI <AULIADIANS311@GMAIL.COM>
2021-12-12 21:11:56 +0700

    load data

--- cluster_app.py
+++ cluster_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
-import joblib
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans as kmeans
+from sklearn.decomposition import PCA
+import plotly.express as px
+import matplotlib.pyplot as plt
+import numpy as np
 
-model = open("kmeans.pkl","rb")
-y_kmeans = joblib.load(model)
+st.header("Report on clustering")
 
-st.title("Clustering")
+data = pd.read_csv("pelanggar_akb.csv")

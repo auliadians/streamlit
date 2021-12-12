f4c1e6b97d6a110f88750d60769109638b9d6ec6
AULIA DIAN SAFITRI <AULIADIANS311@GMAIL.COM>
2021-12-12 20:16:55 +0700

    change name file pickle

--- cluster_app.py
+++ cluster_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = open("clustering.pkl","rb")
+model = open("kmeans.pkl","rb")
 y_kmeans = joblib.load(model)
 
 st.title("Clustering")

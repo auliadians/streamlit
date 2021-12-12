a130632444ef15332f450fc25152c1bf5687b3bc
AULIA DIAN SAFITRI <AULIADIANS311@GMAIL.COM>
2021-12-13 06:47:53 +0700

    model

--- cluster_app.py
+++ cluster_app.py
@@ -18,11 +18,13 @@ st.dataframe(data)
 df = data.drop(data.columns[0], axis = 1)
 scaler = StandardScaler()
 scaled_data = scaler.fit_transform(df)
+
+number_of_clusters = st.slider("number of clusters to display", min_value=4, max_value=48, step=2, value=10)
+
 #creating and fitting the model
-model = kmeans(n_clusters=3, n_init=10).fit_predict(scaled_data)
+model = kmeans(n_clusters=number_of_clusters n_init=10).fit_predict(scaled_data)
 #visualisation of the clusters
 fig_cluster = px.scatter(data_frame=scaled_data,)
 #displaying the clusters
 st.subheader("Displaying the cluster")
 st.plotly_chart(fig_cluster)#for showing plotly figures, we have the function plotly chart and pass our fig_cluster as the parameter
-number_of_clusters = st.slider("number of clusters to display", min_value=4, max_value=48, step=2, value=10)

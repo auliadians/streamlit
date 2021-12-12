ffb5acc08ef841f57abec3ef4ef415b0426ce615
AULIA DIAN SAFITRI <AULIADIANS311@GMAIL.COM>
2021-12-13 06:58:05 +0700

    comma

--- cluster_app.py
+++ cluster_app.py
@@ -20,14 +20,11 @@ scaler = StandardScaler()
 scaled_data = scaler.fit_transform(df)
 
 number_of_clusters = st.slider("number of clusters to display", min_value=4, max_value=48, step=2, value=10)
-st.write("The suggested K Value (By Silhuette Method) is K = " , sil_best_score[0], " with a Value of = ", sil_best_score[1])
-
-number_of_clusters = st.slider("number of clusters to display", min_value=4, max_value=48, step=2, value=10)
 source_df = all_viz_data[number_of_clusters]      
 source = source_df
 st.write("This Silhuette Score for ", number_of_clusters  )
 #creating and fitting the model
-model = kmeans(n_clusters=2 n_init=10).fit_predict(scaled_data)
+model = kmeans(n_clusters=2, n_init=10).fit_predict(scaled_data)
 #visualisation of the clusters
 fig_cluster = px.scatter(data_frame=scaled_data,)
 #displaying the clusters

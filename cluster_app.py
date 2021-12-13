b8d2eb4b43e1d4386eefc4a58c3e65908e36f6fb
AULIA DIAN SAFITRI <AULIADIANS311@GMAIL.COM>
2021-12-13 14:09:05 +0700

    new version

--- cluster_app.py
+++ cluster_app.py
@@ -20,25 +20,14 @@ df = data.drop(data.columns[0], axis = 1)
 scaler = StandardScaler()
 scaled_data = scaler.fit_transform(df)
 
-number_of_clusters = st.slider("number of clusters to display", min_value=2, max_value=6, value=3)
-
 # initialising the PCA
 pca=PCA(n_components=2) #we're calling the PCA method and we're specifying that we want two components.
 #fitting the PCA
 principalComponents = pca.fit_transform(scaled_data)#we're fitting the pca method on our dataset
 #making a dataframe of the principal components
-principalDf = pd.DataFrame(data = principalComponents, columns=['principal_component_1', 'principal_component_2']
+principalDf = pd.DataFrame(data = principalComponents, columns=['pca1', 'pca2']
+
+number_of_clusters = st.slider("number of clusters to display", min_value=2, max_value=6, value=3)
 
 
-#source_df = number_of_clusters     
-#source = source_df
-#st.write("This Silhuette Score for ", number_of_clusters  )
-#creating and fitting the model
-#model = kmeans(n_clusters=number_of_clusters, n_init=10).fit_predict(scaled_data)
 
-
-#visualisation of the clusters
-#fig_cluster = px.scatter(data_frame=scaled_data)
-#displaying the clusters
-#st.subheader("Displaying the cluster")
-#st.plotly_chart(fig_cluster)#for showing plotly figures, we have the function plotly chart and pass our fig_cluster as the parameter

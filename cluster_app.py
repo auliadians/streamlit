10ba7873632b5d547cb8eb830a0bfe650dfb1410
AULIA DIAN SAFITRI <AULIADIANS311@GMAIL.COM>
2021-12-13 14:12:41 +0700

    typo

--- cluster_app.py
+++ cluster_app.py
@@ -21,13 +21,12 @@ scaler = StandardScaler()
 scaled_data = scaler.fit_transform(df)
 
 number_of_clusters = st.slider("number of clusters to display", min_value=2, max_value=6, value=2)
-
 # initialising the PCA
 pca=PCA(n_components=2) #we're calling the PCA method and we're specifying that we want two components.
 #fitting the PCA
 principalComponents = pca.fit_transform(scaled_data)#we're fitting the pca method on our dataset
 #making a dataframe of the principal components
-principalDf = pd.DataFrame(data = principalComponents, columns=['pca1', 'pca2']
+principalDf = pd.DataFrame(data = principalComponents, columns=['pca1', 'pca2'])
 
 
 

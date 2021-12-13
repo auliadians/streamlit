f79805f6d863517713488e63bf88b1aaa0cb4830
AULIA DIAN SAFITRI <AULIADIANS311@GMAIL.COM>
2021-12-13 16:59:04 +0700

    complete

--- cluster.py
+++ cluster.py
@@ -6,7 +6,6 @@ from sklearn.decomposition import PCA
 import plotly.express as px
 import matplotlib.pyplot as plt
 import numpy as np
-import altair as alt
 
 st.header("Report on clustering")
 
@@ -20,8 +19,7 @@ df = data.drop(data.columns[0], axis = 1)
 scaler = StandardScaler()
 scaled_data = scaler.fit_transform(df)
 
-number_of_clusters = st.slider("number of clusters to display", min_value=2, max_value=6, value=2)
-model = kmeans(n_clusters=number_of_clusters, n_init=10).fit_predict(scaled_data)
+model = kmeans(n_clusters=2, n_init=10).fit(scaled_data)
 
 # initialising the PCA
 pca=PCA(n_components=2) #we're calling the PCA method and we're specifying that we want two components.
@@ -37,6 +35,36 @@ fig_cluster = px.scatter(data_frame=principalDf, x='pca1', y='pca2')
 st.subheader("Displaying the cluster")
 st.plotly_chart(fig_cluster)
 
+# list to store the within sum of squared error(inertia) for the different clusters given the respective cluster size
+wss = []
 
+# loop to iterate over the no of clusters and calculate the wss
+for i in range(1,7): # 1 to 10
+  #kmeans
+  fitx = kmeans(n_clusters=i, init='kmeans++', n_init=5, random_state=42).fit(scaled_data)
+  # appending the value
+  wss.append(fitx.inertia_)
 
+#to make a curve that can be passed to a variable, use subplot
+fig, ax = plt.subplots(figsize=(11,8.5))
+plt.plot(range(1,11), wss, 'bx-')
+plt.xlabel('Number of clusters $k$')
+plt.ylabel('Inertia')
+plt.title('The Elbow method showing the optimal $k$')
+
+#displaying the elbow curve
+st.subheader("The Elbow curve")
+st.pyplot(fig)
+
+st.subheader("Getting the input from the user")
+input=st.text_area("Input your values : ", value = "Enter the predictor values")#text_area is used to take input
+
+try:
+    cleaned_input = [float(i.strip()) for i in input.split(", ")]
+    output = model.predict(np.array(cleaned_input).reshape(1, 24))
+
+    st.text("The closest cluster to the data is {}".format(output[0]))#text function is used to display
+    #in the dashboard to directly input values, use control+enter
+except:
+    pass
 
